eb82029e64620f54b9b9b33b4b1aac066aa17ae9
Projects1234w <87812544+Projects1234w@users.noreply.github.com>
2022-02-02 21:07:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+ //funtion paddleInCanvas call
+
+//left paddle
+
+//pc computer paddle
+
+//function midline call
+    
+//funtion drawScore call 
+   
+//function models call  
+   
+//function move call which in very important
+
+//function reset when ball does notcame in the contact of padde
+
+//function midline draw a line in center
+
+//function drawScore show scores
+
+//very important function of this game
+
+//width height of canvas speed of ball
+
+//this function help to not go te paddle out of 
